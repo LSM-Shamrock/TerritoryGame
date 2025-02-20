bdb2399a00b21a8ec3963e02081155340f510d47
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-02-20 16:43:59 +0900

    2025-02-20

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class MapController : MonoBehaviour
 {
-    
+    [SerializeField] private int mapWidth;
+    [SerializeField] private int mapHeight;
+    [SerializeField] private SpriteRenderer tilePrefab;
+    [SerializeField] private SpriteRenderer wallPrefab;
+
+    private void Start()
+    {
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                Instantiate(tilePrefab, new(x, y), new(), transform);
+            }
+        }
+    }
 }
